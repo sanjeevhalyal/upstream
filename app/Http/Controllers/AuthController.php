a8019ae33bf99c67f9398215beca20af6158391a
Monish <monish7792@example.com>
2018-03-30 14:51:32 +0100

    Laravel Socialite added

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+use Socialize;
+use Microsoft\Graph\Graph;
+use Microsoft\Graph\Model;
+use App\User;
+use App\Http\Controllers;
+
+class AuthController extends Controller
+{
+  /**
+     * Redirect the user to the Graph authentication page.
+     *
+     * @return Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialize::with('graph')->redirect();
+
+    }
+
+    /**
+     * Obtain the user information from graph.
+     *
+     * @return Response
+     */
+    public function handleProviderCallback(Request $request)
+    {
+        $user = Socialize::driver('graph')->user();
+        //Check wether the URL is consist of NUIG
+        $authUser = $this->findUser($user);
+        if($authUser)
+        {
+          //login
+          Auth::login($authUser, true);
+          return redirect('/');
+        }
+        else {
+          //registration
+          $us = User::create([
+               'name'     => $user->name,
+               'email'    => $user->email
+           ]);
+           Auth::login($us, true);
+           return redirect('/home');
+        }
+    }
+
+    /**
+     * Check wether the user exists using Microsoft Auth
+     * If it does, Return the user
+     * If it doesnt redirect the user to registration page.
+     * @return Response
+     */
+     public function findUser($us)
+     {
+       $user = User::where('email', $us->email)->first();
+       if($user)
+       {
+         return $user;
+       }
+       return false;
+     }
+
+
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <h1>{{auth()->user()->name}}</h1>
+                    <h3>Welcome to Inventory System</h3>
+                    <h5>Your details.</h5>
+                    <form class="form-horizontal" action="/action_page.php">
+                      <div class="form-group">
+                        <label class="control-label col-sm-2" for="tel">Mobile No:</label>
+                        <div class="col-sm-10">
+                          <input type="tel" class="form-control" id="tel" placeholder="Enter Mobile no" name="tel">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label class="control-label col-sm-2" for="soc">Society:</label>
+                        <div class="col-sm-10">
+                          <input type="text" class="form-control" id="soc" placeholder="Enter Society" name="soc">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label class="control-label col-sm-2" for="post">Position:</label>
+                        <div class="col-sm-10">
+                          <input type="text" class="form-control" id="post" placeholder="Enter Society" name="post">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                          <button type="submit" class="btn btn-default">Submit</button>
+                        </div>
+                      </div>
+                    </form>
+                    {{-- form for storing the Values --}}
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
